31214d2168dce4021851a0a58ebb9611984e30d5
yoon-jae <yoonjae.cho92@gmail.com>
2016-12-18 16:34:19 +0900

    Fix comment

--- MiniC to Ucode/src/UcodeCodeGen.java
+++ MiniC to Ucode/src/UcodeCodeGen.java
@@ -5,7 +5,8 @@ public class UcodeCodeGen {
 	public static void main(String[] args) throws Exception {
         MiniCLexer lexer = new MiniCLexer(new ANTLRFileStream("./Test/test.c"));
 //        MiniCLexer lexer = new MiniCLexer(new ANTLRFileStream("./Test/swap.c"));
-//        MiniCLexer lexer = new MiniCLexer(new ANTLRFileStream("./Test/fibo.c"));		CommonTokenStream tokens = new CommonTokenStream(lexer);
+//        MiniCLexer lexer = new MiniCLexer(new ANTLRFileStream("./Test/fibo.c"));
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		MiniCParser parser = new MiniCParser(tokens);
 		ParseTree tree = parser.program();
 
